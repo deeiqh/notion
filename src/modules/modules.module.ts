d732fb938536827a01756037c83d73a0e2b6181b
Kevin Salazar <deeiqh@gmail.com>
2022-10-01 05:17:22 -0500

    fix: solve providers

--- src/modules/modules.module.ts
+++ src/modules/modules.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { ModulesController } from './modules.controller';
 import { ModulesService } from './modules.service';
 
 @Module({
   controllers: [ModulesController],
-  providers: [ModulesService]
+  providers: [ModulesService, PrismaService],
 })
 export class ModulesModule {}
--- src/programs/programs.module.ts
+++ src/programs/programs.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { ProgramsController } from './programs.controller';
 import { ProgramsService } from './programs.service';
 
 @Module({
   controllers: [ProgramsController],
-  providers: [ProgramsService]
+  providers: [ProgramsService, PrismaService],
 })
 export class ProgramsModule {}
